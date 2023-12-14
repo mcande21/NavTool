c56259eff5685c0b110af4dec3c43fe466f56a66
mcande21 <candersoan@gocva.com>
2023-12-13 21:10:24 -0500

    Initial Commit

---
+++ NavTool/NavToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavToolApp.swift
+//  NavTool
+//
+//  Created by Cooper Anderson on 12/13/23.
+//
+
+import SwiftUI
+
+@main
+struct NavToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
